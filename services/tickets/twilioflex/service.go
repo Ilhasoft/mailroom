51256d4905fb98bd6c0b9c1a3d84a41e86531b2d
Robi9 <robertasousa714@gmail.com>
2023-02-17 13:18:04 -0300

    Go back to single message history and remove sort

--- services/tickets/twilioflex/service.go
+++ services/tickets/twilioflex/service.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"net/url"
 	"path"
-	"sort"
 	"strings"
 	"sync"
 	"time"
@@ -284,11 +283,6 @@ func SendHistory(session flows.Session, contactID flows.ContactID, newFlexChanne
 		logrus.Error(errors.Wrap(err, "failed to get history messages"))
 		return
 	}
-
-	// sort messages by CreatedOn()
-	sort.SliceStable(msgs, func(i, j int) bool {
-		return msgs[i].CreatedOn().Before(msgs[j].CreatedOn())
-	})
 
 	var trace *httpx.Trace
 	historyMsg := ""
